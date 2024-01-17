f4973b2e0b0bf6f768b9d9e018423c3bc789e1e8
Chien <chien@gmail.com>
2024-01-17 16:12:59 +0700

    configmvc

---
+++ DemoSpringmvc/src/main/java/DemoSpringMVC/controller/homeController.java
@@ -0,0 +1,12 @@
+package DemoSpringMVC.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class homeController {
+	@RequestMapping("/")
+	public String index() {
+		return "views.user.index";
+	}
+}
